85cd996a88e4bdc0acc91b2f7bbfe1a60a09f12e
BlueKossa <dexter@stormdal.se>
2024-10-10 14:56:38 +0200

    Made ast a flat tree

--- src/parsing.rs
+++ src/parsing.rs
@@ -2,6 +2,21 @@ use std::iter::Peekable;
 
 use crate::tokenizing;
 
+pub enum AstNodeKind {
+    Expr(Expr),
+}
+
+pub struct AstNode {
+    kind: AstNodeKind,
+    location: NodeLocation,
+}
+
+pub type Ast = Vec<AstNode>;
+
+pub trait IntoNode {
+    fn into_node(self, loc: NodeLocation) -> AstNode;
+}
+
 pub enum BinOpKind {
     Add,
     Sub,
@@ -26,25 +41,39 @@ pub enum ExprKind {
 
 pub struct Expr {
     kind: ExprKind,
-    pos: usize,
+}
+
+impl IntoNode for Expr {
+    fn into_node(self, loc: NodeLocation) -> AstNode {
+        AstNode {
+            kind: AstNodeKind::Expr(self),
+            location: loc,
+        }
+    }
 }
 
 
 
 pub struct Parser<I: Iterator<Item = tokenizing::Token>> {
     tokens: Peekable<I>,
-    pos: usize,
+    current_node_idx: NodeLocation,
 }
 
 impl<I: Iterator<Item = tokenizing::Token>> Parser<I> {
     pub fn new(tokens: I) -> Self {
         Self {
             tokens: tokens.peekable(),
-            pos: 0,
+            current_node_idx: 0,
         }
     }
 
-    pub fn parse_expr
+    pub fn new_node(&mut self, node_kind: impl IntoNode) -> AstNode {
+        let node = node_kind.into_node(self.current_node_idx);
+        self.current_node_idx += 1;
+        node
+    }
+
+    pub fn parse_expr(&mut self, precedence: u8) -> Expr
 
     pub fn parse(&mut self) {
         while let Some(token) = self.tokens.next() {
